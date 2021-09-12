5125a8c34009e62ac3e2fbbae538ed9a7decccff
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-09-12 23:20:13 +0600

    product picture show in display

--- js/app.js
+++ js/app.js
@@ -3,14 +3,16 @@ const loadProducts = () => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
+  
 };
 loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+  console.log(products[2].image);
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
